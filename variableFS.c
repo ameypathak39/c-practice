81da0046be6fc57b1fc929ca273053acd927d932
Ameya <125235218+ameypathak39@users.noreply.github.com>
2024-09-21 23:56:26 +0530

    Create variableFS.c

---
+++ variableFS.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// C format specifiers : 
+int main(){
+    int myNum = 15;
+    float myFloat = 5.09;
+    char myLetter = 'D';
+
+    printf("%d\n", myNum);
+    printf("%f\n", myFloat);
+    printf("%c\n",myLetter);
+    
+    return 0;
+    }
