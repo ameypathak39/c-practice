e5fe064c1bb50539aba7f126234c1d41b5492379
Ameya <125235218+ameypathak39@users.noreply.github.com>
2024-09-21 23:37:52 +0530

    Create variable_ex1.c

---
+++ variable_ex1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    //student data;
+    int studentID = 15;
+    int studentAge = 23;
+    float studentFee = 75.25;
+    char studentGrade = 'B';
+
+    // print Variable;
+    printf("Student ID: %d\n", studentID);
+    printf("Student Age: %d\n", studentAge);
+    printf("Student fee: %f\n",studentFee);
+    printf("student grade : %c",studentGrade);
+    return 0;
+    
+}
