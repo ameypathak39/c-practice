797e6bb28df4386282eb97f2094c1796c68057e9
Ameya <125235218+ameypathak39@users.noreply.github.com>
2024-09-21 23:29:51 +0530

    Create datatype.c

---
+++ datatype.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int myInt = 25;
+    float myFloat = 3.14;
+    double myDouble = 9.87654321;
+    char myChar = 'A';
+    char myString[] = "hello, amey";
+    printf("Integer :%d\n", myInt);
+    printf("Float :%2f\n", myFloat);
+    printf("Double :%8lf\n",myDouble);
+    printf("character : %c\n",myChar);
+    printf("String :%s\n",myString);
+    
+    return 0;
+}
